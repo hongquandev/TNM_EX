4d9cdf7992b8d32d875d5cf9cf4458fef96b6960
Quan Nguyen <hongquandev@gmail.com>
2021-03-04 11:33:02 +0700

    first commit

--- user.php
+++ user.php
@@ -7,56 +7,68 @@ function jsonOutput($data)
 }
 
 // Yêu Cầu: 
-// git clone https://github.com/hongquandev/TNM_EX.git
+// Prepares: git clone https://github.com/hongquandev/TNM_EX.git, localhost, mysql
 // 1. Tạo table user trong user.script.sql, thiết lập localhost và database
 // 2. Sử dụng Mysql Adapter bất kỳ để xử lý trên DB vd (mysql_connect, PDO, mysqli....)
 // 3. Tạo các functions tương ứng với các API yêu cầu dưới đây :
 
 /**
- * @api POST localhost/TNM_Ex/user.php?action=createUser
+ * @api POST TNM_Ex/user.php?action=createUser 
  * @method createUser()
- * @param string $user_id
- * @param string $first_name
- * @param string $last_name
- * @param string $email
- * @param string $phone
- * @param string $status
+ * @param string $user_id (required)
+ * @param string $first_name (required)
+ * @param string $last_name (required)
+ * @param string $email (required)
+ * @param string $phone (optional)
+ * @param string $status (optional)
  * @return array
  */
 
 /**
- * @api GET localhost/TNM_Ex/user.php?action=getUserDetail
+ * ex: http://localhost/TNM_Ex/user.php?action=getUserDetail
+ * @api GET TNM_Ex/user.php?action=getUserDetail
  * @method getUserDetail()
- * @param string $user_id
+ * @param string $user_id (required)
  * @return array
  */
 
 /**
- * @api GET /user.php
+ * @api GET TNM_Ex/user.php?action=getUserList
  * @method getUserList()
- * @param string $user_id
- * @param string $status
- * @param string $page
- * @param string $limit
+ * @param string $user_id (optional)
+ * @param string $status (optional)
+ * @param string $page (optional)
+ * @param string $limit (optional)
  * @return array Users
  */
 
 /**
- * @api POST /user.php
+ * @api POST TNM_Ex/user.php?action=updateUser
  * @method updateUser()
- * @param string $user_id
- * @param string $first_name
- * @param string $last_name
- * @param string $email
- * @param string $phone
- * @param string $status
+ * @param string $user_id (required)
+ * @param string $first_name (required)
+ * @param string $last_name (required)
+ * @param string $email (required)
+ * @param string $phone (optional)
+ * @param string $status (optional)
  * @return array
  */
 
 /**
- * @api POST /user.php
+ * @api POST TNM_Ex/user.php?action=removeUser
  * @method removeUser()
- * @param string $user_id
+ * @param string $user_id (required)
+ * @return array
+ */
+
+/**
+ * @api POST TNM_Ex/user.php?action=searchUser
+ * @method searchUser()
+ * @param string $first_name (required)
+ * @param string $email (optional)
+ * @param string $phone (optional)
+ * @param string $page (optional)
+ * @param string $limit (optional)
  * @return array
  */
 
@@ -67,7 +79,7 @@ function jsonOutput($data)
 function getUserDetail()
 {
     //.....
-
+    var_dump($_GET['user_id']);
     // Example Json Success
     $result = [
         'success' => 1,
@@ -95,4 +107,8 @@ function getUserDetail()
 // END CODE
 
 //
-!empty($_GET['action']) && $_GET['action']();
+if(!empty($_GET['action']) && function_exists($_GET['action'])){
+    $_GET['action']();
+}else{
+    echo "This function is not exist.";
+}
